c8dc478e2cd53ca480dd86d6b3d34b2d30e08708
ykkmr <hiyenim@gmail.com>
2025-12-02 01:45:39 +0900

    Fix testcode OK

--- src/main/kotlin/com/rev/app/config/OpenApiConfig.kt
+++ src/main/kotlin/com/rev/app/config/OpenApiConfig.kt
@@ -1,21 +1,24 @@
 package com.rev.app.config
 
-
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
+import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import io.swagger.v3.oas.annotations.security.SecurityScheme
 import io.swagger.v3.oas.models.OpenAPI
-import io.swagger.v3.oas.models.info.Info
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Profile
 
 @Configuration
-class OpenApiConfig {
-    @Bean
-    fun apiInfo(): OpenAPI = OpenAPI()
-        .info(
-            Info()
-                .title("Community API")
-                .version("v1")
-                .description("Threads, Comments, Bookmarks")
-        )
-}
+@OpenAPIDefinition(
+    info = Info(title = "REV API", version = "v1"),
+    security = [SecurityRequirement(name = "bearerAuth")]
+)
+@SecurityScheme(
+    name = "bearerAuth",
+    type = SecuritySchemeType.HTTP,
+    scheme = "bearer",
+    bearerFormat = "JWT"
+)
+class OpenApiConfig
 
